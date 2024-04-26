1f57ee10b85b78d531870743ffa88beb9ef44583
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-26 23:05:15 +0800

    GetTempFolder

---
+++ QingYi.Core/Temp/GetTempFolder.cs
@@ -0,0 +1,11 @@
+﻿namespace QingYi.Core.Temp
+{
+    public class GetTempFolder
+    {
+        public static string Get()
+        {
+            string tempFolderPath = Path.GetTempPath();
+            return tempFolderPath;
+        }
+    }
+}
