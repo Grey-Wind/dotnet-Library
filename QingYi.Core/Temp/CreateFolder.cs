4d7716dcbc201ec206e8f1acf905e0e953fc6810
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-26 23:56:15 +0800

    create folder in temp

---
+++ QingYi.Core/Temp/CreateFolder.cs
@@ -0,0 +1,10 @@
+﻿namespace QingYi.Core.Temp
+{
+    public class CreateFolder
+    {
+        public static void Create(string newFolderName)
+        {
+            Folder.Create.CreateFolder(newFolderName, GetTempFolder.Get());
+        }
+    }
+}
